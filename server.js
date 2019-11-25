fe6615110bf69afd12f02de59e96ea61067868e1
Salman Khan <salmank@nxn.com.pk>
2019-11-25 15:20:20 +0500

    4th commit

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(config.db.uri, { useNewUrlParser: true, useUnifiedTopology: tru
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
-app.get('/auth', authUser);
+//app.get('/auth', authUser);
 
 
 app.get('/salman', (request, response) => {
@@ -29,21 +29,22 @@ app.get('/', (request, response) => {
 
 app.get('/auth', (req, res) => {
   console.log(req.query.code);
+  res.redirect(307, '/test');
+});
+
+app.post('/test', (req, res) => {
   var params = {
     app_id: '445969309446550',
     app_secret: '291958d2c06000112ee51d34657b6beb',
     grant_type: 'authorization_code',
     redirect_uri: 'https://insta.humwell.com:8888/auth',
-    code: req.query.code
+    code: 'AQCuNZINhXOKGOnD7QKGSLlFw-I1D32ZlO07BmdpEDNKK3D1qwYpcQSRgqMbNW-S6Aj0Q1q4yS8XM8fn3PqZOn-Y1vmQlfMKEHtnDOeF0YMt7YEyLNvg_sltrP-zIZtkC3-NcYnEJfebEO_iUMhG_PWZWpfjGEw5CioITiW8yVo6i__Rg19snWZtQr3L3Z5k_TUXobaOqMDkfyP8EEVZhvAWY5yR_wY9B1CbVcWUDVMKXA'
   }
   axios.post('https://api.instagram.com/oauth/access_token', params).then(response => {
     console.log(response);
     res.send(response.data);
-
-
   }).catch(err => res.status(404).json(err));
-});
-
+})
 
 app.get('/login', function (request, response) {
   response.redirect(config.instagram.auth_url);
