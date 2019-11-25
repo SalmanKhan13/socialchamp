5e417c0a84926b665967da2e0cd6c63b81606400
Salman Khan <salmank@nxn.com.pk>
2019-11-25 14:29:10 +0500

    2nds commit

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var in_client_id = '445969309446550',
   in_client_secret = '291958d2c06000112ee51d34657b6beb',
-  in_redirect_uri = 'https://localhost:8888/handleauth',
+  in_redirect_uri = 'https://insta.humwell.com:8888/handleauth',
   in_auth_url = 'https://api.instagram.com/oauth/authorize?app_id=' + in_client_id +
     '&redirect_uri=' + in_redirect_uri + '&scope=user_profile,user_media' + '&response_type=code'
 
@@ -16,8 +16,8 @@ module.exports = {
     uri: db_uri
   },
   instagram: {
-    client_id: in_client_id,
-    client_secret: in_client_secret,
+    app_id: in_client_id,
+    app_secret: in_client_secret,
     auth_url: in_auth_url
   }
 };
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const morgan = require('morgan');
 const config = require('./config/config');
 const mongoose = require('mongoose');
-var authUser = require('./app/services/authService');
+const authUser = require('./app/services/authService');
+const https = require('https');
+const fs = require('fs');
 
 
 const app = express();
@@ -14,33 +16,95 @@ mongoose.connect(config.db.uri, { useNewUrlParser: true, useUnifiedTopology: tru
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
-app.get('/handleauth', authUser);
+app.get('/auth', authUser);
 
 
-
-app.get('/', function (request, response) {
-  response.send('Server is working')
-});
-app.get('/', function (request, response) {
-  response.sendfile('./public/index.html')
+app.get('/salman', (request, response) => {
+  response.send('Salman is working')
 });
 
-app.get('/handleauth', function (request, response) {
-  response.send(request.query.code);
+app.get('/', (request, response) => {
+  response.sendFile('./public/index.html')
 });
+
+//app.get('/auth',(req, res)=> {
+// console.log(res);
+// res.send(req.query.code);
+//});
+
 
 app.get('/login', function (request, response) {
   response.redirect(config.instagram.auth_url);
 });
 
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/archive/insta.humwell.com/privkey1.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/archive/insta.humwell.com/cert1.pem')
+}
+
+const port = process.env.PORT || 8888;
+
+const server = https.createServer(options, app).listen(port, '0.0.0.0', function () {
+  console.log("Express server listening on port " + port);
+});
 
 
-const PORT = process.env.PORT || 8888;
-app.listen(PORT, err => {
-  if (err) {
-    throw err
-  }
-  console.log(`Server listening on port ${PORT}`)
-})
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const morgan = require('morgan');
+// const config = require('./config/config');
+// const mongoose = require('mongoose');
+// var authUser = require('./app/services/authService').default;
+
+
+// const app = express();
+
+// mongoose.connect(config.db.uri, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('Mongo db connected...')
+//   });
+
+// app.use(morgan('dev'));
+// app.use(express.static(__dirname + '/public'));
+// app.get('/auth', authUser);
+
+
+
+// app.get('/', function (request, response) {
+//   response.send('Server is working')
+// });
+// app.get('/', function (request, response) {
+//   response.sendfile('./public/index.html')
+// });
+
+// app.get('/auth', (req, res) => {
+//   res.send(request.query.code);
+
+// });
+
+// app.get('/login', function (request, response) {
+//   response.redirect(config.instagram.auth_url);
+// });
+
+
+
+// const PORT = process.env.PORT || 8888;
+// app.listen(PORT, err => {
+//   if (err) {
+//     throw err
+//   }
+//   console.log(`Server listening on port ${PORT}`)
+// })
 
 
